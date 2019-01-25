21f880a4fc5676e7b715fa7c223c3d18b92d04b9
Joel <joelverastegui99@gmail.com>
2019-01-25 16:20:43 -0500

    Agregando MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/coffee'
 } else {
-    urlDB = 'mongodb://admin:admin-coffee2019@ds213255.mlab.com:13255/coffee-db'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
